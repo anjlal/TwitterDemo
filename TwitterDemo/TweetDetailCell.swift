1ec29bbca875cbc29a8213c9fd04d285f94870a5
Angie Lal <angielal@gmail.com>
2017-04-15 23:39:08 -0700

    Complete detail view cell layout.

--- TwitterDemo/TweetDetailCell.swift
+++ TwitterDemo/TweetDetailCell.swift
@@ -19,25 +19,54 @@ class TweetDetailCell: UITableViewCell {
     @IBOutlet weak var favImage: UIImageView!
     @IBOutlet weak var retweetCountLabel: UILabel!
     @IBOutlet weak var favCountLabel: UILabel!
+    @IBOutlet weak var retweeterLabel: UILabel!
+    @IBOutlet weak var smallRetweetImage: UIImageView!
+    @IBOutlet weak var timestampLabel: UILabel!
     
+    var user: User?
+    var tweet: Tweet?
+    var isFavorited = false
+    var isRetweeted = false
+
     var tweetData: Tweet? {
         didSet{
-            let user = tweetData?.user
-            nameLabel.text = tweetData?.user?.name as String?
-            tweetLabel.text = tweetData?.text as String?
+            
+            if let retweetedStatus = tweetData?.retweetedStatus {
+                smallRetweetImage.isHidden = false
+                smallRetweetImage.image = UIImage(named: "rtgrey.png")
+                retweeterLabel.text = "\(tweetData?.user?.screenname as String? ?? "") retweeted"
+                retweeterLabel.isHidden = false
+                tweet = Tweet(dictionary: retweetedStatus)
+                user = User(dictionary: retweetedStatus["user"] as! NSDictionary)
+            } else {
+                if tweetData?.user != nil {
+                    user = (tweetData?.user)!
+                    tweet = tweetData!
+                    retweeterLabel.isHidden = true
+                    smallRetweetImage.isHidden = true
+                    
+                }
+            }
+            
+            nameLabel.text = user?.name as String?
+            tweetLabel.text = tweet?.text as String?
+            favCountLabel.text = String(describing: tweet?.favoritesCount ?? 0)
+            retweetCountLabel.text = String(describing: tweet?.retweetCount ?? 0)
+            
+            isFavorited = (tweet?.isFavorited as Bool?)!
             
             if let profileUrl = user?.profileUrl {
                 self.profileImage.setImageWith(profileUrl as URL)
             }
-            profileImage.layer.cornerRadius = 3
-            profileImage.clipsToBounds = true
             
             if let screenname = user?.screenname {
                 screennameLabel.text = String("@\(screenname)")
             }
             
-            retweetCountLabel.text = String(describing: tweetData?.retweetCount ?? 0)
-            favCountLabel.text = String(describing: tweetData?.favoritesCount ?? 0)
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeStyle = DateFormatter.Style.short
+            dateFormatter.dateStyle = DateFormatter.Style.short
+            timestampLabel.text = dateFormatter.string(from: tweetData?.timestamp! as! Date)
             
         }
     }
@@ -48,6 +77,8 @@ class TweetDetailCell: UITableViewCell {
         self.favImage.image = UIImage(named: "favgrey.png")
         self.replyImage.image = UIImage(named: "replygrey.png")
         self.retweetImage.image = UIImage(named: "rtgrey.png")
+        profileImage.layer.cornerRadius = 3
+        profileImage.clipsToBounds = true
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
